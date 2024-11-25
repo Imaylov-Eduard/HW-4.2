24a9c2b5027ae03f1f520bbbad22c92c6728640f
asus2 <izmailoff.ehduard@yandex.ru>
2024-11-25 21:17:48 +0300

    HW-4.2

--- src/Main.java
+++ src/Main.java
@@ -9,25 +9,23 @@ public class Main {
         if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
         } else {
-            System.out.println("Установите версию приложения для Android по ссылке");}
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
 
 
         System.out.println("Задание№2.");
 
-        int clientOS1 = 0;
+        int clientOS1 = 1;
         int clientDeviceYear = 2015;
 
-        if (clientOS1 == 0) {
-            if (clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-            } else {
-                System.out.println("Установите версию приложения для iOS по ссылке");
-            }
+        if (clientDeviceYear < 2015 && clientOS1 == 0) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientDeviceYear >= 2015 && clientOS1 == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientDeviceYear < 2015 && clientOS1 == 1) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
         } else {
-            if (clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке");
-            } else {
-                System.out.println("Установите версию приложения для Android по ссылке");}
+            System.out.println("Установите версию приложения для Android по ссылке");
         }
 
         System.out.println("Задание№3.");
@@ -39,7 +37,8 @@ public class Main {
         } else if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
             System.out.println(year + " год является високосным");
         } else {
-            System.out.println(year + " год не является високосным");}
+            System.out.println(year + " год не является високосным");
+        }
 
         System.out.println("Задание№4.");
 
@@ -87,9 +86,9 @@ public class Main {
             case 9:
             case 10:
             case 11:
-                System.out.println("Осень");
+                System.out.println("Осень.");
                 break;
         }
-
     }
 }
+
